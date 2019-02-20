de745d3dc81bdcb235c0016e9d2626d82fe1dd98
zijie zhao <zhaozijie94@gmail.com>
2019-02-19 22:15:31 -0500

    hw2 commit

--- src/mypkg/my_answers.py
+++ src/mypkg/my_answers.py
@@ -10,22 +10,22 @@ def numbers_and_strings():
     This is to review numbers and strings and basic operations.
     """
     # Write square of 2018
-    x = None
+    x = 2018 ** 2
 
     # Assign a string "Stevens" to a variable y
-    y = None
+    y = "Stevens"
 
     # Repeat variable y 5 times using *
-    z = None
+    z = y * 5
 
     # What is the length of z?
-    length = None
+    length = len(z)
 
     # Concatenate variable y with string " is good"
-    m = None
+    m = y + " is good"
 
     # Replace "good" with "awesome" in variable m and assign it to a new variable n
-    n = None
+    n = m.replace("good", "awesome")
 
     return x, y, z, length, m, n
 
@@ -37,27 +37,30 @@ def lists():
     n = "Stevens is awesome"
 
     # Split variable n on a delimiter 'space' into a list of substrings
-    p = None
+    p = n.split()
 
     # Get all the items except the first of the third substring
-    r = None
+    r = n[0 : 11] + n[12 : ]
 
     # Create a 3 x 3 matrix as nested list such that
     #   first row is [1, 4, 5]
     #   second row is [6, 10, 11]
     #   third row is [12, 17, 38]
-    A = None
+    A = [[1, 4, 5],
+         [6, 10, 11],
+         [12, 17, 38]
+         ]
 
     # Collect the items in the last column of matrix A using list comprehension
-    c = None
+    c = [row[2] for row in A]
 
     # Collect only the even items of the diagonal of matrix A using list comprehension
-    d = None
+    d = [A[i][i] for i in [1, 2]]
 
     # We can convert a single character to its underlying integer code (e.g., its ASCII byte value)
     # by passing it to the built-in ord function. Generate a list of these integers to represent
     # each character of the string "Stevens" using list comprehension.
-    o = None
+    o = [ord(x) for x in "Stevens"]
 
     return p, r, c, d, o
 
@@ -70,28 +73,34 @@ def dictionaries():
     #   fruit => "apple"
     #   quantity => 4
     #   color => "green"
-    f = None
+    f = { 'fruit' : 'apple',
+          'quantity' : 4,
+          'color' : 'green'}
 
     # Get the item in dictionary f that the key "fruit" maps to
-    a = None
+    a = f['fruit']
 
     # Increase the quantity of f by 1
-    # IMPLEMENT IT HERE
+    f['quantity'] += 1
 
     # Create a nested dictionary where:
     #   name => {first_name => "Grace", last_name => "Hopper"} (a dictionary)
     #   jobs => ["scientist", "engineer"] (a list)
     #   age => 85
-    amazing_grace = None
+    amazing_grace = {'name' : {'first_name' : 'Grace',
+                               'last_name' : 'Hopper'},
+                     'jobs' : ["scientist", "engineer"],
+                     'age' : 85
+                    }
 
     # Add "programmer" to the list of jobs Grace has
-    # IMPLEMENT IT HERE
+    amazing_grace['jobs'].append('programmer')
 
     # Get the third job Grace has that you recently added
-    p = None
+    p = amazing_grace['jobs'][2]
 
     # Get the sorted keys of amazing_grace in alphabetically ascending order
-    k = None
+    k = sorted(amazing_grace.keys())
 
     return a, f, p, k
 
